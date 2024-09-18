ed8b2e1d42bab8a2039b6c913b7cbeff3829dcb7
muthukumar.kb <muthukumar.kb@gmail.com>
2024-09-18 18:57:10 +0000

    Create  a blob object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.zip.InflaterInputStream;
 import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Main {
   public static void main(String[] args){
@@ -49,11 +51,54 @@ public class Main {
 
           default -> System.out.println("Unknown type: " + type);
           }
+      }
 
+      case "hash-object" -> {
+        final String type = args[1];
+        switch (type) {
+          case "-w" -> {
+            // Compute Hash
+            final String path = args[2];
+            final File file = new File(path);
+            sha = hash(file);
+            System.out.println(sha);
 
+            // Write to .git/objects
+            File bFile = new File(".git/objects/" + sha.substring(0, 2), sha.substring(2));
+            bFile.getParentFile().mkdirs();
+            bFile.createNewFile();
+            Files.write(bFile.toPath(), (type + "\0" + Files.readString(file.toPath())).getBytes());
+
+            }
+
+          default -> System.out.println("Unknown type: " + type);
+
+          }
+        }
       }
 
       default -> System.out.println("Unknown command: " + command);
     }
   }
+  
+  public static String hash(File file) {
+    try {
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        // Add logic here to read the file contents and update the digest
+        byte[] hashBytes = digest.digest();
+        return bytesToHex(hashBytes);
+    } catch (NoSuchAlgorithmException e) {
+        throw new RuntimeException(e);
+    }
+  }
+  // Helper function to convert byte array to hex string
+  private static String bytesToHex(byte[]bytes) {
+    StringBuilder sb = new StringBuilder();
+    for (byte b : bytes) {
+        sb.append(String.format("%02x", b));
+    }
+    return sb.toString();
+  }
+
+
 }
